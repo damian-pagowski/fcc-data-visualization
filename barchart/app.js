70552fd582113d4e69c57f3d6eb3558ad392c6b5
Damian <d.pagowski@gmail.com>
2020-01-02 10:39:46 +0700

    barchart cahllenge - tests passed

--- barchart/app.js
+++ barchart/app.js
@@ -1,126 +1,142 @@
-const width = Math.floor(window.innerWidth - 200);
-const height = Math.floor(window.innerHeight - 200);
-const MARGIN = 25;
-const MARGIN_LEFT = 40;
+const LEFT_MARGIN = 50;
+const WIDTH = Math.floor(window.innerWidth - 200);
+const HEIGHT = Math.floor(window.innerHeight - 200);
+
+const tooltip = d3
+  .select("#barchart")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("opacity", 0);
+
+function showToolTip(year, value, data, x, y) {
+  x = Math.floor(x);
+  y = Math.floor(y);
+  tooltip
+    .html(
+      year +
+        "<br>" +
+        "$" +
+        value.toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,") +
+        " Billion"
+    )
+    .attr("data-date", data)
+    .style("opacity", 1)
+    .style("left", x + "px")
+    .style("top", y + "px");
+}
 
 const container = d3
   .select("#barchart")
   .append("svg")
-  .attr("width", width + 50)
-  .attr("height", height + 50)
-  .style("background-color", "pink");
-
-const url =
-  "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-
-d3.json(url)
-  .then(data => doThings(data))
-  .catch(error => console.log(error));
+  .attr("width", WIDTH + 100)
+  .attr("height", HEIGHT + 60);
 
 function addTitle() {
   d3.select("svg")
     .append("text")
-    .attr("x", width / 2)
+    .attr("x", WIDTH / 2)
     .attr("y", "30")
     .attr("id", "title")
     .style("font-size", "2rem")
     .text("gdp");
 }
-function addAxisY(yAxisScale) {
-  const yAxis = d3.axisLeft(yAxisScale);
-  d3.select("svg")
-    .append("g")
-    .call(yAxis)
-    .attr("id", "y-axis")
-    .attr("transform", `translate(${MARGIN_LEFT}, 25)`);
-}
 
-function formatDate(date) {
-  var d = new Date(date),
-    month = "" + (d.getMonth() + 1),
-    day = "" + d.getDate(),
-    year = d.getFullYear();
+d3.json(
+  "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+)
+  .then(data => {
+    addTitle();
+    const barWidth = WIDTH / data.data.length;
+    const years = formatDates(data);
 
-  if (month.length < 2) month = "0" + month;
-  if (day.length < 2) day = "0" + day;
+    const yearsDate = data.data.map(item => new Date(item[0]));
 
-  return [year, month, day].join("-");
+    const xMax = new Date(d3.max(yearsDate));
+
+    const xScale = d3
+      .scaleTime()
+      .domain([d3.min(yearsDate), xMax])
+      .range([0, WIDTH]);
+
+    addAxisX(xScale);
+
+    const GDP = data.data.map(item => item[1]);
+    const scaledGDP = calculateScaledGdp(GDP);
+    addAxisY(GDP);
+
+    d3.select("svg")
+      .selectAll("rect")
+      .data(scaledGDP)
+      .enter()
+      .append("rect")
+      .attr("data-date", (d, i) => data.data[i][0])
+      .attr("data-gdp", (d, i) => data.data[i][1])
+      .attr("class", "bar")
+      .attr("x", (d, i) => xScale(yearsDate[i]))
+      .attr("y", d => HEIGHT - d)
+      .attr("width", barWidth)
+      .attr("height", d => d)
+      .attr("class", "bar")
+      .attr("transform", "translate(60, 0)")
+      .on("mouseover", (d, i) =>
+        showToolTip(years[i], GDP[i], data.data[i][0], i * barWidth, HEIGHT - d)
+      )
+      .on("mouseout", d => tooltip.style("opacity", 0));
+  })
+  .catch(error => console.log(error));
+
+function calculateScaledGdp(GDP) {
+  const gdpMax = d3.max(GDP);
+  const linearScale = d3
+    .scaleLinear()
+    .domain([0, gdpMax])
+    .range([0, HEIGHT]);
+  const scaledGDP = GDP.map(item => linearScale(item));
+  return scaledGDP;
 }
 
 function addAxisX(xScale) {
   const xAxis = d3.axisBottom().scale(xScale);
-  d3.select("svg")
+  container
     .append("g")
     .call(xAxis)
     .attr("id", "x-axis")
-    .attr("transform", `translate(${MARGIN}, ${height + MARGIN})`);
+    .attr("transform", `translate(${60}, ${HEIGHT})`);
 }
 
-function doThings(data) {
-  // us-1
-  addTitle();
+function addAxisY(GDP) {
+  const gdpMax = d3.max(GDP);
 
-  // us-9
-  const dates = data.data.map(item => new Date(item[0]));
-  const dateMax = new Date(d3.max(dates));
-  const dateMin = new Date(d3.min(dates));
-
-  const xScale = d3
-    .scaleTime()
-    .domain([dateMin, dateMax])
-    .range([0, width]);
-
-  const gdp = data.data.map(item => item[1]);
-  const maxGdp = d3.max(gdp);
-
-  const linearScale = d3
-    .scaleLinear()
-    .domain([0, maxGdp])
-    .range([0, height]);
-
-  const scaledGdp = gdp.map(item => linearScale(item));
-
-  const barWidth = width / dates.length;
-
-  const months = [
-    "JAN",
-    "FEB",
-    "MAR",
-    "APR",
-    "MAY",
-    "JUN",
-    "JUL",
-    "AUG",
-    "SEP",
-    "OCT",
-    "NOV",
-    "DEC",
-  ];
-
-  //   const prettyDates = scaledGdp.map( d => formatDate())
-  d3.select("svg")
-    .selectAll("rect")
-    .data(scaledGdp)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => xScale(dates[i]) + MARGIN_LEFT)
-    .attr("y", d => height - d + MARGIN)
-    .attr("width", barWidth)
-    .attr("height", d => d)
-    .attr("class", "bar")
-    .attr("data-date", (d, i) => data.data[i][0])
-    .attr("data-gdp", (d, i) => data.data[i][1])
-    .append("title")
-    .text((d, i) => `${formatDate(dates[i])} : ${d} USD`)
-    .attr("id", "tooltip")
-    .attr("data-date", (d, i) => formatDate(dates[i]));
-
-  addAxisX(xScale);
-  // Y
   const yAxisScale = d3
     .scaleLinear()
-    .domain([0, maxGdp])
-    .range([height, 0]);
+    .domain([0, gdpMax])
+    .range([HEIGHT, 0]);
+  const yAxis = d3.axisLeft(yAxisScale);
+  container
+    .append("g")
+    .call(yAxis)
+    .attr("id", "y-axis")
+    .attr("transform", "translate(60, 0)");
+}
 
-  addAxisY(yAxisScale);
+const months = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+function formatDates(data) {
+  return data.data.map(item => {
+    const monthNum = item[0].substring(5, 7) - 1;
+    return months[monthNum] + " " + item[0].substring(0, 4);
+  });
 }
